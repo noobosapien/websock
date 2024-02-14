acbbbb85a742d737c2ca628c4cde1060baaf7bab
Noobosapien <migirulz44@gmail.com>
2024-02-14 19:31:42 +1300

    Websock test

---
+++ server.js
@@ -0,0 +1,12 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 3030 });
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", function message(data) {
+    console.log("received: %s", data);
+    ws.send(data.toString());
+  });
+
+  ws.send("something");
+});
